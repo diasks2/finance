e3f44f9380cf4ab1edd2cb7a1ed680dbb2ec0308
Kevin <diasks2@gmail.com>
2013-02-19 23:02:30 +0900

    Begin to add double entry logic to transactions controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,14 +5,26 @@ class TransactionsController < ApplicationController
   end
 
   def create
-    @transaction = Transaction.new(params[:transaction])
-    # o =  [('a'..'z'),('A'..'Z')].map{|i| i.to_a}.flatten
-    # string  =  (0...50).map{ o[rand(o.length)] }.join
-    if @transaction.save
-      redirect_to @transaction, notice: "New Transaction Successfully Created"
-    else
-      render :new
-    end
+    @transaction_a = Transaction.new(params[:transaction])
+    @transaction_b = Transaction.new(params[:transaction])
+  
+    # check to make sure amounts of both records = 0
+    if @transaction_a.amount.value == @transaction_b.amount.value
+  		# create hash string for both records
+    	o = [('a'..'z'),('A'..'Z')].map{|i| i.to_a}.flatten
+    	string = (0...50).map{ o[rand(o.length)] }.join
+    	@transaction_a.update_attribute :unique_hash, string
+    	@transaction_b.update_attribute :unique_hash, string
+	    
+	    if @transaction_a.save && @transaction_b.save
+	      redirect_to transactions_path, notice: "New Transaction Successfully Created"
+	    else
+	      render :new
+	    end
+	else
+        raise "Error - transactions to not balance"
+    end    
+
   end
 
   def edit
