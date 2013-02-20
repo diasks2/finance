b4e521c28e79366c1f6d7a0ced4161df4c3c80bb
Kevin <diasks2@gmail.com>
2013-02-21 08:55:22 +0900

    Begin to add totals to group index

---
+++ app/helpers/groups_helper.rb
@@ -0,0 +1,10 @@
+module GroupsHelper
+  def group_yen_total(group_id)
+  	 Transaction.includes(:category, :group).where("amount > ?", 0).where("currency = ?", "JPY").where("groups.id = ?", group_id).sum("amount")
+  end
+  def group_dollar_total(group_id)
+  	 Transaction.includes(:category, :group).where("amount > ?", 0).where("currency = ?", "USD").where("groups.id = ?", group_id).sum("amount").to_d / 100
+  end
+  def group_converted_total(group_id)
+  end
+end
