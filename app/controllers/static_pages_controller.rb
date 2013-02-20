577db48a85365934cca8cc9656b84e7bd52e71f2
Kevin <diasks2@gmail.com>
2013-02-20 22:41:47 +0900

    Work on homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,13 +3,41 @@ class StaticPagesController < ApplicationController
   def home
   end
 
-  def graph
-  	@transactions = Transaction.where("amount > ?", 0).where("category_id != ?", 1).where("category_id != ?", 2).where("category_id != ?", 3).where("category_id != ?", 4).where("category_id != ?", 5).all
+  def graph1
+    #@transactions = Transaction.includes(:category, :group).where("amount > ?", 0).where("category_id != ?", 1).where("category_id != ?", 2).where("category_id != ?", 3).where("category_id != ?", 4).where("category_id != ?", 5).group("category_id")
+    @transactions = Transaction.includes(:category, :group).where("amount > ?", 0).where("groups.id != ?", 1).group("category_id")
     respond_to do |format|
       format.json{
         render :json => JSON.generate(@transactions.as_json(:include => [:category, :group]))
       }
     end
   end
+  def graph2
+  	#@transactions = Transaction.includes(:category, :group).where("amount > ?", 0).where("groups.id != ?", 1).group("groups.name")
+    #@transactions = Transaction.includes(:category, :group).where("amount > ?", 0).where("groups.id != ?", 1).group("groups.id").sum("amount")
+    #respond_to do |format|
+     # format.json{
+     #   render :json => JSON.generate(@transactions.as_json(:include => [:category, :group]))
+     # }
+    #end
+    @transactions = Transaction.includes(:category, :group).where("amount > ?", 0).where("groups.id != ?", 1).group("groups.id").sum("amount")
+    respond_to do |format|
+      format.html
+      format.json do
+        render :json => custom_json_for(@transactions)
+      end
+    end
+  end
+
+private
+def custom_json_for(value)
+  list = value.map do |k, v|
+    { :group_id => k,
+      :amount => v    
+    }
+  end
+  list.to_json
+end
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # -*- encoding : utf-8 -*-
 Finance::Application.routes.draw do
   root to: 'static_pages#home'
-  match '/graph', to: 'static_pages#graph'
+  match '/graph1', to: 'static_pages#graph1'
+  match '/graph2', to: 'static_pages#graph2'
   resources :accounts
   resources :transactions
   resources :categories
