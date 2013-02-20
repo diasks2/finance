4be6185cc1824bdf452025f3a8b34266626dbc59
Kevin <diasks2@gmail.com>
2013-02-20 19:10:28 +0900

    Work on JSON response

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,14 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def graph
+  	@transactions = Transaction.where("amount > ?", 0).where("category_id != ?", 1).where("category_id != ?", 2).where("category_id != ?", 3).where("category_id != ?", 4).where("category_id != ?", 5).all
+    respond_to do |format|
+      format.json{
+        render :json => JSON.generate(@transactions.as_json(:include => [:category, :group]))
+      }
+    end
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
 Finance::Application.routes.draw do
   root to: 'static_pages#home'
+  match '/graph', to: 'static_pages#graph'
   resources :accounts
   resources :transactions
   resources :categories
